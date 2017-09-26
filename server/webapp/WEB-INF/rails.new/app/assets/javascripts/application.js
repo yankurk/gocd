77ad7a5efc8a6cbb3d7775ee7c32e3f04d5e6a42
yankurk <ankurk@thoughtworks.com>
2017-09-26 18:16:49 +0530

    Creates a rails app scaffolding to migrate rails from 4.0.4 to 4.2.9

--- server/webapp/WEB-INF/rails.new/app/assets/javascripts/application.js
+++ server/webapp/WEB-INF/rails.new/app/assets/javascripts/application.js
@@ -1,16 +1,73 @@
+/*
+ * Copyright 2015 ThoughtWorks, Inc.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
 // This is a manifest file that'll be compiled into application.js, which will include all the files
 // listed below.
 //
 // Any JavaScript/Coffee file within this directory, lib/assets/javascripts, vendor/assets/javascripts,
-// or any plugin's vendor/assets/javascripts directory can be referenced here using a relative path.
+// or vendor/assets/javascripts of plugins, if any, can be referenced here using a relative path.
 //
 // It's not advisable to add code directly here, but if you do, it'll appear at the bottom of the
 // compiled file.
 //
-// Read Sprockets README (https://github.com/rails/sprockets#sprockets-directives) for details
-// about supported directives.
-//
-//= require jquery
-//= require jquery_ujs
-//= require turbolinks
-//= require_tree .
+// Read Sprockets README (https://github.com/sstephenson/sprockets#sprockets-directives) for details
+// about supported directives
+
+//= require "lib/es5-shim.min.js"
+//= require "lib/jquery-1.7.2.js"
+//= require "lib/jquery.url-1.0.js"
+//= require "lib/jquery-pinOnScroll.js"
+//= require "lib/jquery_no_conflict.js"
+//= require "lib/prototype-1.6.0.js"
+//= require "lib/scriptaculous-1.8.0.js"
+//= require "lib/bootstrap-2.3.2.min.js"
+//= require "lib/angular-1.0.8.js"
+//= require "lib/angular-resource.1.0.8.min.js"
+//= require "lib/effects-1.8.0.js"
+//= require "lib/accordion-2.0.js"
+//= require "lib/controls-1.8.0.js"
+//= require "lib/dragdrop-1.8.0.js"
+//= require "lib/highcharts-2.3.3.min.js"
+//= require "lib/jquery-ui-1.7.3.custom.min.js"
+//= require "lib/jquery.autocomplete-1.1.js"
+//= require "lib/jquery.ba-throttle-debounce-1.1.min.js"
+//= require "lib/jquery.dirtyform.js"
+//= require "lib/jquery.highlight-3.0.js"
+//= require "lib/jquery.tipTip-1.3.js"
+//= require "lib/jquery.treeview-1.5pre.js"
+//= require "lib/jquery.validate-1.5.5.js"
+//= require "lib/modalbox-1.6.1.js"
+//= require "lib/slider-1.8.0.js"
+//= require "lib/trimpath-template-1.0.38.js"
+//= require "lib/ui.core-1.7.3.js"
+//= require "lib/ui.dialog-1.7.3.js"
+//= require "lib/lodash.js"
+//= require "lib/moment-2.18.1.js"
+//= require "lib/moment-duration-format-1.3.0.js"
+//= require "lib/humanize-for-gocd.js"
+//= require "lib/pako_inflate-1.0.5.js"
+//= require "lib/often-0.3.2.js"
+//= require "lib/component-emitter-1.2.1.js"
+//= require "ansi_up.js"
+//= require "crel.js"
+//= require "json_to_css.js"
+//= require "util.js"
+//= require "micro_content_popup.js"
+//= require "ajax_popup_handler.js"
+//= require "compare_pipelines.js"
+//= require "console_log_tailing.js"
+//= require "js-routes"
+//= require_directory .
--- server/webapp/WEB-INF/rails.new/spec/support/rspec_config.rb
+++ server/webapp/WEB-INF/rails.new/spec/support/rspec_config.rb
@@ -1,3 +1,5 @@
+require 'capybara/rspec'
+
 RSpec.configure do |config|
 # Use color not only in STDOUT but also in pagers and files
   config.tty = true
