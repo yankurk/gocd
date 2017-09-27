67ed6f1e23e7b9e4c5d45c6cb9b58c63b61849f0
Ketan Padegaonkar <KetanPadegaonkar@gmail.com>
2017-09-27 08:42:08 +0530

    Copy over assets file

--- server/webapp/WEB-INF/rails.new/config/initializers/assets.rb
+++ server/webapp/WEB-INF/rails.new/config/initializers/assets.rb
@@ -8,4 +8,7 @@ Rails.application.config.assets.version = '1.0'
 
 # Precompile additional assets.
 # application.js, application.css, and all non-JS/CSS in app/assets folder are already added.
-# Rails.application.config.assets.precompile += %w( search.js )
+Rails.application.config.assets.precompile += %w( lib/d3-3.1.5.min.js css/*.css)
+Rails.application.config.assets.precompile += %w(frameworks.css single_page_apps/pipeline_configs.css single_page_apps/agents.css single_page_apps/elastic_profiles.css single_page_apps/preferences.css single_page_apps/auth_configs.css single_page_apps/roles.css single_page_apps/plugins.css)
+Rails.application.config.assets.precompile += %w(*.svg *.eot *.woff *.ttf *.gif *.png *.ico)
+Rails.application.config.assets.precompile += %w( new-theme.css )
