3da103f01253d14fd0bb438ed042091e424fd17d
Bhupendrakumar Piprava <bdpiparva@gmail.com>
2017-11-06 17:35:52 +0530

    Fixed admin/pipelines_controller_spec.rb

--- server/webapp/WEB-INF/rails.new/spec/controllers/admin/pipelines_controller_spec.rb
+++ server/webapp/WEB-INF/rails.new/spec/controllers/admin/pipelines_controller_spec.rb
@@ -38,43 +38,6 @@ describe Admin::PipelinesController do
     allow(controller).to receive(:package_definition_service).with(no_args).and_return(@package_definition_service = StubPackageDefinitionService.new)
   end
 
-  describe "routes" do
-    it "should match /edit" do
-      expect({:get => "/admin/pipelines/foo.bar/general"}).to route_to(:controller => "admin/pipelines", :action => "edit", :pipeline_name => 'foo.bar', :current_tab => 'general', :stage_parent => "pipelines")
-    end
-
-    it "should match /update" do
-      expect({:put => "/admin/pipelines/foo.baz/general"}).to route_to(:controller => "admin/pipelines", :action => "update", :pipeline_name => 'foo.baz', :current_tab => 'general', :stage_parent => "pipelines")
-    end
-
-    it "should match /pause_info" do
-      expect({:get => "/admin/pipelines/foo.baz/pause_info.json"}).to route_to(:controller => "admin/pipelines", :action => "pause_info", :pipeline_name => 'foo.baz', :format => "json")
-      expect(pause_info_refresh_path(:pipeline_name => 'foo.baz')).to eq("/admin/pipelines/foo.baz/pause_info.json")
-    end
-
-    it "should match /new" do
-      expect({:get => "/admin/pipeline/new"}).to route_to(:controller => "admin/pipelines", :action => "new")
-      expect(pipeline_new_path).to eq("/admin/pipeline/new")
-      expect(pipeline_new_path(:group => "foo.bar")).to eq("/admin/pipeline/new?group=foo.bar")
-    end
-
-    it "should match /create" do
-      expect({:post => "/admin/pipelines"}).to route_to(:controller => "admin/pipelines", :action => "create")
-      expect(pipeline_create_path).to eq("/admin/pipelines")
-    end
-
-    it "should match /clone" do
-      expect({:get => "/admin/pipeline/foo.bar/clone"}).to route_to(:controller => "admin/pipelines", :action => "clone", :pipeline_name => 'foo.bar')
-      expect(pipeline_clone_path(:pipeline_name => "foo.bar")).to eq("/admin/pipeline/foo.bar/clone")
-    end
-
-    it "should match /save_clone" do
-      expect({:post => "/admin/pipeline/save_clone"}).to route_to(:controller => "admin/pipelines", :action => "save_clone")
-      expect(pipeline_save_clone_path).to eq("/admin/pipeline/save_clone")
-    end
-
-  end
-
   describe "pause_info" do
     before(:each) do
       pipeline_config = PipelineConfigMother.pipelineConfigWithMingleConfiguration("HelloWorld", "http://mingleurl.com:7823", "go", "'status' > 'In Dev'")
