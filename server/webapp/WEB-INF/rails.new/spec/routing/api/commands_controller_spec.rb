b6de7c6a85f14e187e7223acc32967cddce9c2b5
Bhupendrakumar Piprava <bdpiparva@gmail.com>
2017-11-06 15:29:42 +0530

    Fixed api/commands_controller_spec.rb

--- server/webapp/WEB-INF/rails.new/spec/routing/api/commands_controller_spec.rb
+++ server/webapp/WEB-INF/rails.new/spec/routing/api/commands_controller_spec.rb
@@ -17,20 +17,13 @@
 require 'rails_helper'
 
 describe Api::CommandsController do
-  before :each do
-    @command_repository_service = stub_service(:command_repository_service)
-  end
 
-  describe "actions" do
-    describe "reload_cache" do
-      it "should reload the cache" do
-        expect(@command_repository_service).to receive(:reloadCache)
-
-        post :reload_cache, params: {:no_layout => true}
-
-        expect(response.status).to eq(200)
-        expect(response.code).to eq("200")
-      end
+  describe "routes" do
+    it "should generate path for command snippet cache reload " do
+      expect_any_instance_of(HeaderConstraint).to receive(:matches?).with(any_args).and_return(true)
+      expect(:post => '/api/admin/command-repo-cache/reload').to route_to(:no_layout => true, :controller => "api/commands", :action => "reload_cache")
+      expect(admin_command_cache_reload_path).to eq("/api/admin/command-repo-cache/reload")
     end
   end
+
 end
