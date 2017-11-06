e542cb2fef9bc36a5c881fd44a07d0be01c9f8c9
Jyoti Singh <jyotisingh7@gmail.com>
2017-11-06 18:58:17 +0530

    Moved routing specs for spec/controllers/admin/pipelines_snippet_controller_spec.rb

--- server/webapp/WEB-INF/rails.new/spec/controllers/admin/pipelines_snippet_controller_spec.rb
+++ server/webapp/WEB-INF/rails.new/spec/controllers/admin/pipelines_snippet_controller_spec.rb
@@ -27,28 +27,6 @@ describe Admin::PipelinesSnippetController do
     allow(controller).to receive(:go_config_service).and_return(@go_config_service)
   end
 
-  describe "routes" do
-    it "should resolve the route to partial config page" do
-      expect({:get => "/admin/pipelines/snippet"}).to route_to(:controller => "admin/pipelines_snippet", :action => "index")
-      expect(pipelines_snippet_path).to eq("/admin/pipelines/snippet")
-    end
-
-    it "should resolve route to get group xml" do
-      expect({:get => "/admin/pipelines/snippet/foo.bar"}).to route_to(:controller => "admin/pipelines_snippet", :action => "show", :group_name => "foo.bar")
-      expect(pipelines_snippet_show_path(:group_name => 'foo.bar')).to eq("/admin/pipelines/snippet/foo.bar")
-    end
-
-    it "should resolve route to save group xml" do
-      expect({:put => "/admin/pipelines/snippet/foo.bar"}).to route_to(:controller => "admin/pipelines_snippet", :action => "update", :group_name => "foo.bar")
-      expect(pipelines_snippet_update_path(:group_name => 'foo.bar')).to eq("/admin/pipelines/snippet/foo.bar")
-    end
-
-    it "should resolve route to edit group xml" do
-      expect({:get => "/admin/pipelines/snippet/foo.bar/edit"}).to route_to(:controller => "admin/pipelines_snippet", :action => "edit", :group_name => "foo.bar")
-      expect(pipelines_snippet_edit_path(:group_name => 'foo.bar')).to eq("/admin/pipelines/snippet/foo.bar/edit")
-    end
-  end
-
   describe "actions" do
     before :each do
       expect(controller).to receive(:populate_config_validity).and_return(true)
