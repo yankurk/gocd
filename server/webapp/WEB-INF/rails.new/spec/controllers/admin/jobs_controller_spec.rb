b8fe006b253e0d87a386fdbeceb5a8950c83c1c0
Bhupendrakumar Piprava <bdpiparva@gmail.com>
2017-11-06 16:35:42 +0530

    Fixed admin/jobs_controller_spec.rb

--- server/webapp/WEB-INF/rails.new/spec/controllers/admin/jobs_controller_spec.rb
+++ server/webapp/WEB-INF/rails.new/spec/controllers/admin/jobs_controller_spec.rb
@@ -38,40 +38,6 @@ describe Admin::JobsController do
     allow(controller).to receive(:pluggable_task_service).and_return(@pluggable_task_service)
   end
 
-  describe "routes" do
-    it "should resolve new" do
-      expect({:get => "/admin/pipelines/dev/stages/test.1/jobs/new"}).to route_to(:controller => "admin/jobs", :action => "new", :pipeline_name => "dev", :stage_name => "test.1", :stage_parent => "pipelines")
-      expect({:get => "/admin/templates/dev/stages/test.1/jobs/new"}).to route_to(:controller => "admin/jobs", :action => "new", :pipeline_name => "dev", :stage_name => "test.1", :stage_parent => "templates")
-      expect(admin_job_new_path(:pipeline_name => "foo.bar", :stage_name => "test.1", :stage_parent => "pipelines")).to eq("/admin/pipelines/foo.bar/stages/test.1/jobs/new")
-      expect(admin_job_new_path(:pipeline_name => "foo.bar", :stage_name => "test.1", :stage_parent => "templates")).to eq("/admin/templates/foo.bar/stages/test.1/jobs/new")
-    end
-
-    it "should resolve create" do
-      expect({:post => "/admin/pipelines/dev/stages/test.1/jobs"}).to route_to(:controller => "admin/jobs", :action => "create", :pipeline_name => "dev", :stage_name => "test.1", :stage_parent => "pipelines")
-      expect(admin_job_create_path(:pipeline_name => "foo.bar", :stage_name => "test.1", :stage_parent => "pipelines")).to eq("/admin/pipelines/foo.bar/stages/test.1/jobs")
-    end
-
-    it "should resolve destroy" do
-      expect({:delete => "/admin/pipelines/dev/stages/test.1/job/job.1"}).to route_to(:controller => "admin/jobs", :action => "destroy", :pipeline_name => "dev", :stage_name => "test.1", :job_name=> "job.1", :stage_parent => "pipelines")
-    end
-
-    it "should generate index" do
-      expect({:get => "/admin/pipelines/dev/stages/test.1/jobs"}).to route_to(:controller => "admin/jobs", :action => "index", :pipeline_name => "dev", :stage_name => "test.1", :stage_parent => "pipelines")
-      expect(admin_job_listing_path(:pipeline_name => "foo.bar", :stage_name => "test.1", :stage_parent => "pipelines")).to eq("/admin/pipelines/foo.bar/stages/test.1/jobs")
-    end
-
-    it "should generate destroy" do
-      # Cannot have route_for for DELETE as route_for does not honor the :method => :delete attribute
-      expect(admin_job_delete_path(:pipeline_name => "foo.bar", :stage_name => "test.1", :job_name => "job.1", :stage_parent => "pipelines")).to eq("/admin/pipelines/foo.bar/stages/test.1/job/job.1")
-    end
-
-    it "should generate route for tabs" do
-      expect({:get => "/admin/pipelines/dev/stages/test.1/job/job.1/tabs"}).to route_to(:controller => "admin/jobs", :action => "edit", :pipeline_name => "dev", :stage_name => "test.1", :job_name=> "job.1", :stage_parent => "pipelines", :current_tab => "tabs")
-      expect(admin_job_edit_path(:pipeline_name => "foo.bar", :stage_name => "foo.bar", :job_name => "foo.bar", :current_tab => "tabs", :stage_parent => "templates")).to eq("/admin/templates/foo.bar/stages/foo.bar/job/foo.bar/tabs")
-      expect(admin_job_edit_path(:pipeline_name => "foo.bar", :stage_name => "foo.bar", :job_name => "foo.bar", :current_tab => "tabs", :stage_parent => "pipelines")).to eq("/admin/pipelines/foo.bar/stages/foo.bar/job/foo.bar/tabs")
-    end
-  end
-
   describe "action" do
     before(:each) do
       @cruise_config = BasicCruiseConfig.new()
