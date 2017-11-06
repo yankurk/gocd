a9f476192275d2769579b79bab9392b6b096edd9
Jyoti Singh <jyotisingh7@gmail.com>
2017-11-06 15:21:08 +0530

    Moved routing specs for spec/controllers/environments_controller_spec.rb

--- server/webapp/WEB-INF/rails.new/spec/controllers/environments_controller_spec.rb
+++ server/webapp/WEB-INF/rails.new/spec/controllers/environments_controller_spec.rb
@@ -73,47 +73,6 @@ describe EnvironmentsController do
       expect(assigns[:environments]).to eq(["environment-1", "environment-2"])
     end
 
-    it "should match /environments defaulting to html format" do
-      expect(:get => "/environments").to route_to({:controller => "environments", :action => 'index', :format => :html})
-    end
-
-    it "should match /environments defaulting to json format" do
-      expect(:post => "/environments.json").to route_to({:controller => "environments", :action => 'index', :format => "json"})
-    end
-
-    it "should match /new" do
-      expect(:get => "/environments/new").to route_to({:controller => "environments", :action => 'new', :no_layout => true})
-    end
-
-    it "should match /create" do
-      expect(:post => "/environments/create").to route_to({:controller => "environments", :action => 'create', :no_layout => true})
-    end
-
-    it "should match /update" do
-      expect(:put => "/environments/foo").to route_to({:no_layout=>true, :controller => "environments", :action => 'update', :name => 'foo'})
-      expect(:put => "/environments/foo.bar.baz").to route_to({:no_layout=>true, :controller => "environments", :action => 'update', :name => 'foo.bar.baz'})
-    end
-
-    it "should match /show" do
-      expect(:get => "/environments/foo/show").to route_to({:controller => "environments", :action => 'show', :name => 'foo'})
-      expect(:get => "/environments/foo.bar.baz/show").to route_to({:controller => "environments", :action => 'show', :name => 'foo.bar.baz'})
-    end
-
-    it "should match /edit/pipelines" do
-      expect(:get => "/environments/foo/edit/pipelines").to route_to({:controller => "environments", :action => 'edit_pipelines', :name => 'foo', :no_layout => true})
-      expect(:get => "/environments/foo.bar.baz/edit/pipelines").to route_to({:controller => "environments", :action => 'edit_pipelines', :name => 'foo.bar.baz', :no_layout => true})
-    end
-
-    it "should match /edit/agents" do
-      expect(:get => "/environments/foo/edit/agents").to route_to({:controller => "environments", :action => 'edit_agents', :name => 'foo', :no_layout => true})
-      expect(:get => "/environments/foo.bar.baz/edit/agents").to route_to({:controller => "environments", :action => 'edit_agents', :name => 'foo.bar.baz', :no_layout => true})
-    end
-
-    it "should match /edit/variables" do
-      expect(:get => "/environments/foo/edit/variables").to route_to({:controller => "environments", :action => 'edit_variables', :name => 'foo', :no_layout => true})
-      expect(:get => "/environments/foo.bar.baz/edit/variables").to route_to({:controller => "environments", :action => 'edit_variables', :name => 'foo.bar.baz', :no_layout => true})
-    end
-
     it "should create a new environment" do
       user = com.thoughtworks.go.server.domain.Username.new(CaseInsensitiveString.new('user_foo'))
       allow(controller).to receive(:current_user).and_return(user)
